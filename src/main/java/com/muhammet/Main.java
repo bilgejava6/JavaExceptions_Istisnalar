a43ab83a90b27557d037c79a845675630d9fd886
Muhammet Ali KAYA <info@beemeapp.com>
2023-01-03 12:30:06 +0300

    Initial commit

---
+++ src/main/java/com/muhammet/Main.java
@@ -0,0 +1,22 @@
+package com.muhammet;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("*********************************");
+        System.out.println("*******  HESAP MAKİNESİ    ******");
+        System.out.println("*********************************");
+        System.out.println("1- Toplama");
+        System.out.println("2- Cıkartma");
+        System.out.println("3- Carpma");
+        System.out.println("4- Bölme");
+        System.out.println("0- CIKIS");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Lütfen seçiniz.....: ");
+        int secim = sc.nextInt();
+
+
+    }
+}
