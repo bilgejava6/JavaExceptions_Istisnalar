6857a30d9757bf8df2abd704e067767cb5e4b3a4
Muhammet Ali KAYA <info@beemeapp.com>
2023-01-04 12:25:37 +0300

    04.01.2023 günsonu

--- src/main/java/com/muhammet/Main.java
+++ src/main/java/com/muhammet/Main.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        /**
+         * Uygulamalarımız her daim sorun çıkartma kapasitesine sahiptir. bu nedenle
+         * özellikle kullanıcı ile etkileşime girdiğimiz tüm kodlamaları dikkatle inceleyip
+         * hata yakalama işlemlerine öncelik vermeliyiz.
+         * Syntax:
+         * try{ -> try keyword u kullanılır.
+         *  --> body, hatanın olacağını düşündüşünüz kod bloğunu buraya yazıyorsunuz.
+         * }catch(Exception ex){ -> catch, bir istisna olduğu zaman bunu yakalacak olan kısım
+         *  --> yakalanan hata, hata türüne göre işleme tabi tutulur. istisnanın türü catch
+         *      parantezleri içinde talep edilir.(Exception, RunetimeException v.s.)
+         *      Bir hata olduğunda ne yapılması gerektiği burada işlenir.
+         * }finaly{ --> hata olsun yada olmasın yürütülmesi gereken işlemler, adımlar var ise bunlar
+         *              burada kodlanmalıdır.         *
+         * }
+         */
         System.out.println("*********************************");
         System.out.println("*******  HESAP MAKİNESİ    ******");
         System.out.println("*********************************");
@@ -15,7 +29,15 @@ public class Main {
         System.out.println("0- CIKIS");
         Scanner sc = new Scanner(System.in);
         System.out.print("Lütfen seçiniz.....: ");
-        int secim = sc.nextInt();
+        try{
+            int secim = sc.nextInt();
+
+        }catch (Exception exception){
+            System.out.println("Lütfen rakam giriniz...");
+        }
+
+
+        System.out.println("PROGRAM SONLANDI.");
 
 
     }
